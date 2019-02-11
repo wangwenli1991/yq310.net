b0029a046894ebab9ba4f05e2cf7f40d1dfe72d3
wangwenli1991 <1527179450@qq.com>
2019-02-11 10:05:16 +0800

    Update index.blade.php

--- resources/views/hunli/index.blade.php
+++ resources/views/hunli/index.blade.php
@@ -35,7 +35,7 @@
     <!-- header -->
     <header>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="index.html">工业皮带网</a>
+        <a class="navbar-brand" href="">工业皮带网</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
             aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
